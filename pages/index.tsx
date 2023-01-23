f7677e500dd83e8f912c438136aff4b179c09898
BuzzKill Master <BuzzKillMaster@proton.me>
2023-01-23 13:40:09 +0100

    prevent crashing when user enters invalid ip

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,19 +26,27 @@ export default function Home() {
 
 
     const searchIPAddress = async (query?: string) => {
-        const url = "https://ipapi.co" + (typeof query === "undefined" ? "" : "/" + query) + "/json/"
-        const response = await fetch(url)
-        const data = await response.json()
+        try {
+            const url = "https://ipapi.co" + (typeof query === "undefined" ? "" : "/" + query) + "/json/"
+            const response = await fetch(url)
+            const data = await response.json()
 
-        setResultData({
-            IPAddress: data["ip"],
-            location: [data["city"] + ",", data["postal"], data["country_name"]].join(" "),
-            timezone: data["timezone"],
-            isp: data["org"]
-        })
+            // @ts-ignore
+            if (data["latitude"] === undefined) throw Error()
+            if (data["longitude"] === undefined) throw Error()
 
-        setLatitude(data["latitude"])
-        setLongitude(data["longitude"])
+            setResultData({
+                IPAddress: data["ip"],
+                location: [data["city"] + ",", data["postal"], data["country_name"]].join(" "),
+                timezone: data["timezone"],
+                isp: data["org"]
+            })
+
+            setLatitude(data["latitude"])
+            setLongitude(data["longitude"])
+        } catch (e) {
+            alert("Something went wrong, please try again.")
+        }
     }
 
     return (
