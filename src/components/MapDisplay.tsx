4835d36bfdfb1842e54510cc1537dca2ba254964
BuzzKill Master <BuzzKillMaster@proton.me>
2023-02-26 08:58:25 +0100

    remove unnecessary effect call to ensure code readability and recentering when the same value is entered twice in a row

--- src/components/MapDisplay.tsx
+++ src/components/MapDisplay.tsx
@@ -2,14 +2,13 @@ import {MapContainer, Marker, Popup, TileLayer, useMap, ZoomControl} from 'react
 import 'leaflet/dist/leaflet.css'
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css'
 import "leaflet-defaulticon-compatibility";
-import {useEffect} from "react";
 import Coordinates from "@/types/Coordinates";
 
 export default function MapDisplay(props: Coordinates) {
     const hasLocation = props.latitude !== 0 || props.longitude !== 0
 
     return (
-        <MapContainer center={[props.latitude, props.longitude]} zoom={hasLocation ? 10 : 3} scrollWheelZoom={false} zoomControl={false}
+        <MapContainer center={[props.latitude, props.longitude]} zoom={3} scrollWheelZoom={false} zoomControl={false}
                       className={"w-full min-h-[500px] flex-grow z-0"}>
             <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -31,13 +30,7 @@ function AutomaticRecentering(props: {
     longitude: number
     hasLocation: boolean
 }) {
-    const map = useMap()
-
-    useEffect(() => {
-        if (props.hasLocation) {
-            map.setView([props.latitude, props.longitude], 10)
-        }
-    }, [props.latitude, props.longitude, props.hasLocation, map])
+   useMap().setView([props.latitude, props.longitude], props.hasLocation ? 10 : 3)
 
     return null
 }
